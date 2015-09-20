e677e4381c45f3a9667913c562499d906153138d
Phalanstere <burckhardt@ludicmedia.de>
2015-09-20 19:07:29 +0200

    Test

---
+++ test/test.validate.js
@@ -0,0 +1,20 @@
+/* global describe, it, require */
+'use strict';
+
+// MODULES //
+
+var // Expectation library:
+    chai = require( 'chai' );
+
+// VARIABLES //
+
+var expect = chai.expect,
+    assert = chai.assert,
+    should = chai.should();
+
+
+expect(4+5).equal(9);
+
+describe("Tags", function(){
+ 
+});
